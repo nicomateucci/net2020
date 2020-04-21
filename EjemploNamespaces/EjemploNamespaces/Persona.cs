8fb0c66d07f1ef77266b4e1578d1b9f60169e0a2
nicomateucci <nico-mateucci@hotmail.com>
2020-04-21 13:41:49 -0300

    Agregue proyectos

---
+++ EjemploNamespaces/EjemploNamespaces/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NamespaceDeClasePersona
+{
+    public class Persona
+    {
+        //Defino una persona con atributos
+        //nombre, apellido y edad
+        private string _nombre;
+        private string _apellido;
+        private int _edad;
+
+        //Constructor, se llama cuando haces el "new"
+        // Ejemplo: Persona per = new Persona();
+        public Persona()
+        {
+            this.Apellido = "Messi";
+        }
+
+        public string Nombre
+        {
+            set { _nombre = value; }
+            get { return _nombre; }
+        }
+        public string Apellido
+        {
+            set { _apellido = value; }
+            get { return _apellido; }
+        }
+        public int Edad
+        {
+            set{ _edad = value; }
+            get { return _edad; }
+        }
+    }
+}
---
+++ EjercicioFacturacion/EjercicioFacturacion/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace EjercicioFacturacion
+{
+    public class Producto
+    {
+        private int _codigo;
+        private float _precio;
+        private string _descripcion;
+        private int _stock;
+
+
+
+        public Producto()
+        {
+        }
+
+        public int Codigo { get => _codigo; set => _codigo = value; }
+        public float Precio { get => _precio; set => _precio = value; }
+        public string Descripcion { get => _descripcion; set => _descripcion = value; }
+        public int Stock { get => _stock; set => _stock = value; }
+    }
+}

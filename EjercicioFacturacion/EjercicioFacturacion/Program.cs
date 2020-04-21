727f92f72815153901601d0d16a3b6d09240e8e2
nicomateucci <nico-mateucci@hotmail.com>
2020-04-21 13:16:43 -0300

    Subi Facturacion y Namespaces

---
+++ EjercicioFacturacion/EjercicioFacturacion/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace EjercicioFacturacion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Producto[] productos = new Producto[100];
+            Cliente[] clientes = new Cliente[45];
+
+            Console.WriteLine("Ingrese el nombre de los 100 productos");
+            string nom = Console.ReadLine();
+            for (int i = 0; i < 100; i++)
+            {
+                Producto p = new Producto();
+                p.Codigo = i;
+                p.Descripcion ="Producto: " + nom + i.ToString();
+                p.Precio = i + 1 * 10;
+                p.Stock = 50;
+                productos[i] = p;
+            }
+            Console.WriteLine("Ingrese el nombre de los 45 clientes");
+            string nombre = Console.ReadLine();
+            for (int i = 0; i < 45; i++)
+            {
+                Cliente c = new Cliente();
+                c.Codigo = i;
+                c.Nombre = nombre + i.ToString();
+                c.Direccion = "Direccion de " + nombre + i.ToString();
+                clientes[i] = c;
+            }
+
+            // Articulos vendidos al cliente
+            Console.WriteLine("Ingrese codigo de cliente");
+            int cod = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nombre de cliente seleccionado: " + clientes[cod].Nombre);
+
+            // Permito hacer 3 ingresos de articulos
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Ingrese el codigo de articulo");
+                int codArt = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la cantidad de articulos vendidos");
+                int cant = int.Parse(Console.ReadLine());
+
+                //Verificar que halla stock
+                if (productos[codArt].Stock >= cant)
+                {
+                    //Restar al stock la nueva compra
+                    productos[codArt].Stock -= cant;
+                    // Actualizar monto del cliente
+                    clientes[i].MontoTotal += productos[codArt].Precio * cant;
+                }
+                Console.WriteLine("Factura ---------------------------");
+                Console.WriteLine("Cliente:" + clientes[i].Nombre);
+                Console.WriteLine("Direccion:" + clientes[i].Direccion);
+                Console.WriteLine("Monto total:" + clientes[i].MontoTotal);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# net2020
-Ejemplos de codigo en C# de la materia Tecnologias de Desarrollo de Software IDE, UTN.
+<h1 align="center">
+  <img src="imagenes/UTN.png" alt="logo_utn">
+</h1>
+<h5 align="center">Universidad Tecnologica Nacional</h5>
+
+## Materia: Técnicas y Tecnologías de Desarrollo  Software IDE 2020
+
+# Entorno 🛠️
+
+* **Microsoft Visual Studio 2019**
+    * Proyecto de Consola
+    * Bibliotecas de Clases
+* **Campus Virtual UTN**
+
+
+## Ejempo definicion de una clase en C#:
+```
+using System;
+
+namespace NamespaceDeClasePersona
+{
+    public class Persona
+    {
+        //Defino una persona con atributos
+        //nombre, apellido y edad
+        private string _nombre;
+        private string _apellido;
+        private int _edad;
+
+        //Constructor, se llama cuando haces el "new"
+        // Ejemplo: Persona per = new Persona();
+        public Persona()
+        {
+          // Por defecto se inicia con el apellido Messi
+            this.Apellido = "Messi";
+        }
+
+        public string Nombre
+        {
+            set { _nombre = value; }
+            get { return _nombre; }
+        }
+        public string Apellido
+        {
+            set { _apellido = value; }
+            get { return _apellido; }
+        }
+        public int Edad
+        {
+            set{ _edad = value; }
+            get { return _edad; }
+        }
+    }
+}
+
+```
+
+
+## Autor
+
+* **Nicolas Mateucci** - *Trabajo Practico TTADS Frontend* - [Github oficial ⚡️](https://github.com/nicomateucci)
+
+--------------------------------------------------------
+
+### Plantillas README
+
+[Ejemplos **muy buenos** de uso de Markdown Langauge](https://github.com/ricval/Documentacion/blob/master/Guias/GitHub/mastering-markdown.md#ejemplos)
+
+[Modelo de plantilla para hacer un buen README ⚡️](https://gist.github.com/Villanuevand/6386899f70346d4580c723232524d35a)
+
+
+
+🚀 📋 📦 🛠️ 📄 🎁 📢 🍺 🤓 🔧
